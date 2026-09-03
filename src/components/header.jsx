3e2fc23eee192d1c070127b7f75a72b3d0332046
Louis <louis.dubois@epitech.eu>
2026-09-03 18:56:14 +0200

    ADD: react-icons

--- src/components/header.jsx
+++ src/components/header.jsx
@@ -1,4 +1,5 @@
 import Logo from '../assets/cv_buildor_logo.png';
+import { FaFileDownload } from 'react-icons/fa';
 
 export default function Header() {
     return (
@@ -9,16 +10,21 @@ export default function Header() {
                         <img
                             alt=""
                             src={Logo}
-                            className="h-8 w-auto"
+                            className="h-10 w-auto"
                         />
                         <span className="ml-3 text-white text-xl font-bold">CV Buildor</span>
                     </a>
                 </div>
                 <div>
-                    <button className="text-white font-bold">New CV</button>
+                    <button className="text-xl text-white font-bold border-4 border-white rounded-[15px] p-2">New CV</button>
                 </div>
                 <div>
-                    <button className="text-white font-bold">Download</button>
+                    <button
+                        className="text-xl text-white font-bold flex items-center gap-1"
+                    >
+                        <FaFileDownload size={24} />
+                         Download
+                  </button>
                 </div>
             </nav>
         </header>
