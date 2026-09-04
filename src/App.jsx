708b4d08c6210d3e4a799b8fc36a1cb5cbe094ab
Louis <louis.dubois@epitech.eu>
2026-09-04 10:46:13 +0200

    ADD: set app structure

--- src/App.jsx
+++ src/App.jsx
@@ -2,8 +2,12 @@ import Header from './components/header';
 
 export default function App() {
     return (
-        <>
+       <div className="flex flex-col min-h-screen">
             <Header />
-        </>
+            <main className="flex-1 grid grid-cols-[2fr_3fr] gap-6 bg-gray-400">
+                <div></div>
+                <div></div>
+            </main>
+        </div>
     );
 }
