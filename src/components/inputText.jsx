c2d25fb0c602c51d225bfd8bd380577db353e65b
Louis <louis.dubois@epitech.eu>
2026-09-10 13:58:20 +0200

    ADD: InputText

---
+++ src/components/inputText.jsx
@@ -0,0 +1,14 @@
+export default function InputText({ placeholder = "", value, onChange }) {
+    function handleChange(e) {
+        onChange(e.target.value);
+    }
+
+    return (
+        <input
+            className="w-80 h-10 p-3 border border-black rounded-[5px] focus:outline-none focus:ring-1"
+            placeholder={placeholder}
+            value={value}
+            onChange={handleChange}
+        />
+    )
+}
