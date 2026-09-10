2f8a3999a029644558894314b1bc523a919f73a1
Louis <louis.dubois@epitech.eu>
2026-09-10 14:19:46 +0200

    ADD: InputEmail

--- src/components/inputText.jsx
+++ src/components/inputText.jsx
@@ -5,6 +5,7 @@ export default function InputText({ placeholder = "", value, onChange }) {
 
     return (
         <input
+            type="text"
             className="w-80 h-10 p-3 border border-black rounded-[5px] focus:outline-none focus:ring-1"
             placeholder={placeholder}
             value={value}
