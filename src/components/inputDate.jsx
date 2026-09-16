2b43514e23d78dbc53323c501bc8a282cee709dd
Louis <louis.dubois@epitech.eu>
2026-09-16 12:02:38 +0200

    FIX: date -> only month/year

--- src/components/inputDate.jsx
+++ src/components/inputDate.jsx
@@ -2,22 +2,23 @@ import { useState } from "react";
 
 export default function InputDate({ placeholder = "", value, onChange }) {
     const [error, setError] = useState("");
-    
+
     function handleChange(e) {
         const newVal = e.target.value;
-        const regex = /^(19|20)\d{2}-(0[1-9]|1[0-2])-(0[1-9]|[12][0-9]|3[01])$/;
+        const regex = /^(19|20)\d{2}-(0[1-9]|1[0-2])$/;
 
-        if (newVal && !regex.test(newVal))
-            setError("Invalid date format");
-        else
+        if (newVal && !regex.test(newVal)) {
+            setError("Format invalide. Utilisez YYYY-MM (ex: 2026-09)");
+        } else {
             setError("");
+        }
         onChange(e.target.value);
     }
 
     return (
         <>
             <input
-                type="date"
+                type="month"
                 className="w-80 h-10 p-3 border border-black rounded-[5px] focus:outline-none focus:ring-1"
                 placeholder={placeholder}
                 value={value}
