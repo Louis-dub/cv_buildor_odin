f8a0be9880fb2ee28c5856209a45fc42a1b577f1
Louis <louis.dubois@epitech.eu>
2026-09-16 12:04:14 +0200

    ADD: class Experience

---
+++ src/features/xpCreator.jsx
@@ -0,0 +1,20 @@
+import InputText from "../components/inputText";
+import InputTextArea from "../components/inputTextArea";
+import InputDate from "../components/inputDate";
+import { useState } from "react";
+
+export class Experience {
+    constructor(name, date,  description) {
+        this.name = name;
+        this.date = date;
+        this.description = description;
+    }
+}
+
+export function XpCreator() {
+    
+
+    return (
+        <></>
+    );
+}
