69da839a0d179603a79fa5bea71270a6f7c64b7e
Louis <louis.dubois@epitech.eu>
2026-09-17 17:33:25 +0200

    FIX: inputDate for month and year

--- src/components/inputDate.jsx
+++ src/components/inputDate.jsx
@@ -8,11 +8,12 @@ export default function InputDate({ placeholder = "", value, onChange }) {
         const regex = /^(19|20)\d{2}-(0[1-9]|1[0-2])$/;
 
         if (newVal && !regex.test(newVal)) {
-            setError("Format invalide. Utilisez YYYY-MM (ex: 2026-09)");
+            setError("Invalid format. Use YYYY-MM (ex: 2026-09)");
         } else {
             setError("");
         }
-        onChange(e.target.value);
+        const formattedDate = new Date(newVal).toLocaleDateString('en-En', { month: 'long', year: 'numeric' });
+        onChange(formattedDate);
     }
 
     return (
