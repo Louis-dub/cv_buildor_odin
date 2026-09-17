c9535d8d024a62f90a8179aa6d66cecd6353b83a
Louis <louis.dubois@epitech.eu>
2026-09-17 18:00:31 +0200

    ADD: xp creator component

--- src/components/inputDate.jsx
+++ src/components/inputDate.jsx
@@ -7,24 +7,24 @@ export default function InputDate({ placeholder = "", value, onChange }) {
         const newVal = e.target.value;
         const regex = /^(19|20)\d{2}-(0[1-9]|1[0-2])$/;
 
-        if (newVal && !regex.test(newVal)) {
+        if (newVal && !regex.test(newVal))
             setError("Invalid format. Use YYYY-MM (ex: 2026-09)");
-        } else {
+        else
             setError("");
-        }
-        const formattedDate = new Date(newVal).toLocaleDateString('en-En', { month: 'long', year: 'numeric' });
-        onChange(formattedDate);
+        onChange(newVal);
     }
 
     return (
         <>
-            <input
-                type="month"
-                className="w-80 h-10 p-3 border border-black rounded-[5px] focus:outline-none focus:ring-1"
-                placeholder={placeholder}
-                value={value}
-                onChange={handleChange}
-            />
+            <div className="flex items-center gap-2 w-80 justify-between">
+                <span>{placeholder}</span>
+                <input
+                    type="month"
+                    className="w-[200px] h-10 p-3 border border-black rounded-[5px] focus:outline-none focus:ring-1"
+                    value={value}
+                    onChange={handleChange}
+                />
+            </div>
             {error && <p className="text-red-500 text-sm mt-1">{error}</p>}
         </>
     );
--- src/features/xpCreator.jsx
+++ src/features/xpCreator.jsx
@@ -2,19 +2,87 @@ import InputText from "../components/inputText";
 import InputTextArea from "../components/inputTextArea";
 import InputDate from "../components/inputDate";
 import { useState } from "react";
+import { FaPlus, FaTrashAlt } from "react-icons/fa";
 
-export class Experience {
-    constructor(name, date,  description) {
-        this.name = name;
-        this.date = date;
-        this.description = description;
+export default function XpCreator({ xps, onXpsChange }) {
+    function handleAddXp() {
+        const newXps = [...xps, { name: "", start: "", end: "", description: "" }];
+        onXpsChange(newXps);
     }
-}
 
-export function XpCreator() {
-    
+    function handleSetNameJob(index, name) {
+        const tempXps = [...xps];
+        tempXps[index] = { ...tempXps[index], name: name };
+        onXpsChange(tempXps);
+    }
+
+    function handleSetDateJob(index, date, type) {
+        const tempXps = [...xps];
+        if (type === 0) {
+            tempXps[index] = { ...tempXps[index], start: date };
+        } else {
+            tempXps[index] = { ...tempXps[index], end: date };
+        }
+        onXpsChange(tempXps);
+    }
+
+    function handleRemoveInput(index) {
+        const newXp = [...xps];
+        newXp.splice(index, 1);
+        onXpsChange(newXp); 
+    }
 
     return (
-        <></>
+        <>
+            {xps.map((xp, index) => (
+                <div
+                    key={index}
+                    className="mt-2 mb-4 border border-black rounded-[15px] p-4 flex flex-col items-center gap-2 bg-gray-200"
+                >
+                    <div className="flex items-center w-[100%] pr-2 pl-2 justify-between">
+                        <h1 className="text-center font-2xl font-bold">New Experince</h1>
+                        <button
+                            onClick={() => handleRemoveInput(index)}
+                            className="hover:scale-105 active:scale-95 transition-all ease-out"
+                        >
+                            <FaTrashAlt />
+                        </button>
+                    </div>
+                    <InputText
+                        placeholder="Experience Name"
+                        value={xp.name}
+                        onChange={(value) => handleSetNameJob(index, value)}
+                    />
+                    <InputDate
+                        placeholder="Start Date"
+                        value={xp.start}
+                        onChange={(value) => handleSetDateJob(index, value, 0)}
+                    />
+                    <InputDate
+                        placeholder="End Date"
+                        value={xp.end}
+                        onChange={(value) => handleSetDateJob(index, value, 1)}
+                    />
+                    <InputTextArea
+                        placeholder="Description"
+                        value={xp.description}
+                        onChange={(value) => {
+                            const tempXps = [...xps];
+                            tempXps[index] = { ...tempXps[index], description: value };
+                            onXpsChange(tempXps);
+                        }}
+                    />
+                </div>
+            ))}
+            <div className="mt-2 mb-4 border border-black rounded-[15px] p-4 flex flex-col items-center gap-2 bg-gray-200">
+                <button
+                    onClick={handleAddXp}
+                    className="w-80 text-white bg-gray-900 rounded-[10px] p-2 flex items-center justify-center gap-1 hover:scale-105 active:scale-95 transition-all ease-out"
+                >
+                    <FaPlus />
+                    Add Experience
+                </button>
+            </div>
+        </>
     );
 }
